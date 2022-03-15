8b137ce3641728e49a45cab6baac505942087d9d
Cerdtruir <ghoornabeel@gmail.com>
2022-03-15 13:24:34 +0200

    Add post function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+async function post(name, score) {
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/3k90Dr27MKo8G1oyFr1s/scores/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        user: name,
+        score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    },
+  );
+}
